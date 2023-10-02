332feebd53f1b3c4879ba06e1e979d3939f468c4
yun-ujel <dhall2022@elam.co.uk>
2023-10-02 11:51:25 +0100

    Updated TestShaderGraph
    Added Diffuse and Specular lighting

--- Assets/TestShaderGraph/CustomLighting.hlsl
+++ Assets/TestShaderGraph/CustomLighting.hlsl
@@ -2,19 +2,66 @@
 #define CUSTOM_LIGHTING_INCLUDED
 
 struct CustomLightingData {
+    // Position and orientation
+    float3 normalWS;
+    float3 viewDirectionWS;
+
     // Surface attributes
     float3 albedo;
+    float smoothness;
 };
 
-float3 CalculateCustomLighting(CustomLightingData d) {
-    return d.albedo;
+float GetSmoothnessPower(float rawSmoothness){
+    return exp2(10 * rawSmoothness + 1);
 }
 
-void CalculateCustomLighting_float(float3 Albedo,
+#ifndef SHADERGRAPH_PREVIEW
+float3 CustomLightHandling(CustomLightingData d, Light light) {
+
+    float3 radiance = light.color;
+
+    float diffuse = saturate(dot(d.normalWS, light.direction));
+    float specularDot = saturate(dot(d.normalWS, normalize(light.direction + d.viewDirectionWS)));
+    float specular = pow(specularDot, GetSmoothnessPower(d.smoothness)) * diffuse;
+
+    float3 color = d.albedo * radiance * (diffuse + specular);
+
+    return color;
+}
+#endif
+
+float3 CalculateCustomLighting(CustomLightingData d) {
+    
+    #ifdef SHADERGRAPH_PREVIEW
+
+    float3 lightDir = float3(0.5, 0.5, 0);
+    float intensity = saturate(dot(d.normalWS, lightDir)) +
+        pow(saturate(dot(d.normalWS, lightDir)), GetSmoothnessPower(d.smoothness));
+
+    return d.albedo * intensity;
+
+    #else
+    
+    // Get the main light. Located in URP/ShaderLibrary/Lighting.hlsl
+    Light mainLight = GetMainLight();
+
+    float3 color = 0;
+    // Shade the main light
+    color += CustomLightHandling(d, mainLight);
+
+    return color;
+    #endif
+}
+
+void CalculateCustomLighting_float(float3 Normal, float3 ViewDirection, 
+    float3 Albedo, float Smoothness,
     out float3 Color) {
 
     CustomLightingData d;
+    d.normalWS = Normal;
+    d.viewDirectionWS = ViewDirection;
     d.albedo = Albedo;
+    d.smoothness = Smoothness;
 
     Color = CalculateCustomLighting(d);
 }
