ef0f84d19eb6a41c6588954dc5a0d8849b912367
yun-ujel <dhall2022@elam.co.uk>
2023-10-03 12:45:44 +0100

    Updated TestShaderGraph
    - Added Receiving Shadows (from directional lights as well as point/spot lights)
    - Added Ambient Occlusion and other baked lightmap functionality

--- Assets/TestShaderGraph/CustomLighting.hlsl
+++ Assets/TestShaderGraph/CustomLighting.hlsl
@@ -1,14 +1,30 @@
 #ifndef CUSTOM_LIGHTING_INCLUDED
 #define CUSTOM_LIGHTING_INCLUDED
 
+// This is a neat trick to work around a bug in the shader graph when
+// enabling shadow keywords. Created by @cyanilux
+// https://github.com/Cyanilux/URP_ShaderGraphCustomLighting
+#ifndef SHADERGRAPH_PREVIEW
+    #include "Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/Includes/ShaderPass.hlsl"
+    #if (SHADERPASS != SHADERPASS_FORWARD)
+        #undef REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR
+    #endif
+#endif
+
 struct CustomLightingData {
     // Position and orientation
+    float3 positionWS;
     float3 normalWS;
     float3 viewDirectionWS;
+    float4 shadowCoord;
 
     // Surface attributes
     float3 albedo;
     float smoothness;
+    float ambientOcclusion;
+
+    float3 bakedGI;
+
 };
 
 float GetSmoothnessPower(float rawSmoothness){
@@ -16,9 +32,15 @@ float GetSmoothnessPower(float rawSmoothness){
 }
 
 #ifndef SHADERGRAPH_PREVIEW
+float3 CustomGlobalIllumination(CustomLightingData d) {
+    float3 indirectDiffuse = d.albedo * d.bakedGI * d.ambientOcclusion;
+
+    return indirectDiffuse;
+}
+
 float3 CustomLightHandling(CustomLightingData d, Light light) {
 
-    float3 radiance = light.color;
+    float3 radiance = light.color * (light.distanceAttenuation * light.shadowAttenuation);
 
     float diffuse = saturate(dot(d.normalWS, light.direction));
     float specularDot = saturate(dot(d.normalWS, normalize(light.direction + d.viewDirectionWS)));
@@ -43,25 +65,71 @@ float3 CalculateCustomLighting(CustomLightingData d) {
     #else
     
     // Get the main light. Located in URP/ShaderLibrary/Lighting.hlsl
-    Light mainLight = GetMainLight();
+    Light mainLight = GetMainLight(d.shadowCoord, d.positionWS, 1);
 
-    float3 color = 0;
+    // In mixed subtractive baked lights, the main light must be subtracted
+    // from the bakedGI value. This function in URP/ShaderLibrary/Lighting.hlsl takes care of that.
+    MixRealtimeAndBakedGI(mainLight, d.normalWS, d.bakedGI);
+    float3 color = CustomGlobalIllumination(d);
     // Shade the main light
     color += CustomLightHandling(d, mainLight);
+
+    #ifdef _ADDITIONAL_LIGHTS
+        // Shade additional cone and point lights. Functions in URP/ShaderLibrary/Lighting.hlsl
+        uint numAdditionalLights = GetAdditionalLightsCount();
+        for (uint lightI = 0; lightI < numAdditionalLights; lightI++) {
+            Light light = GetAdditionalLight(lightI, d.positionWS, 1);
+            color += CustomLightHandling(d, light);
+        }
+    #endif
 
     return color;
     #endif
 }
 
-void CalculateCustomLighting_float(float3 Normal, float3 ViewDirection, 
-    float3 Albedo, float Smoothness,
+void CalculateCustomLighting_float(float3 Position, float3 Normal, float3 ViewDirection, 
+    float3 Albedo, float Smoothness, float AmbientOcclusion,
+    float2 LightmapUV,
     out float3 Color) {
 
     CustomLightingData d;
+    d.positionWS = Position;
     d.normalWS = Normal;
     d.viewDirectionWS = ViewDirection;
     d.albedo = Albedo;
     d.smoothness = Smoothness;
+    d.ambientOcclusion = AmbientOcclusion;
+
+#ifdef SHADERGRAPH_PREVIEW
+    // In preview, there's no shadows or bakedGI
+    d.shadowCoord = 0;
+    d.bakedGI = 0;
+#else
+    // Calculate the main light shadow coord
+    // There are two types depending on if cascades are enabled
+    float4 positionCS = TransformWorldToHClip(Position);
+    #if SHADOWS_SCREEN
+        d.shadowCoord = ComputeScreenPos(positionCS);
+    #else
+        d.shadowCoord = TransformWorldToShadowCoord(Position);
+    #endif
+
+    // The following URP functions and macros are all located in
+    // URP/ShaderLibrary/Lighting.hlsl
+    // Technically, OUTPUT_LIGHTMAP_UV, OUTPUT_SH, VertexLighting and ComputeFogFactor
+    // should be called in the vertex function of the shader. However, as of
+    // 2021.1, we do not have access to custom interpolators in the shader graph.
+
+    // The lightmap UV is usually in TEXCOORD1
+    // If lightmaps are disabled, OUTPUT_LIGHTMAP_UV does nothing
+    float2 lightmapUV;
+    OUTPUT_LIGHTMAP_UV(LightmapUV, unity_LightmapST, lightmapUV);
+    // Samples spherical harmonics, which encode light probe data
+    float3 vertexSH;
+    OUTPUT_SH(Normal, vertexSH);
+    // This function calculates the final baked lighting from light maps or probes
+    d.bakedGI = SAMPLE_GI(lightmapUV, vertexSH, Normal);
+#endif
 
     Color = CalculateCustomLighting(d);
 }
