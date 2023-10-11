a2d7049e7f7615ffd519329e93c9c1af8514612d
yun-ujel <dhall2022@elam.co.uk>
2023-10-11 11:58:23 +0100

    Updated XRPullInteractable
    Reorganised Files:
    - Moved Controller model into Visuals/Models/Samples
    - Moved XRPullInteractable into Scripts/XR Interactables
    - Moved Outline Materials into Materials/Global/Outlines
    - Removed SelectTest
    
    Updated XRPullInteractable:
    - now derived from XRGrabInteractable, so no conflicts with other interactable scripts
    - now freezes object in place when grabbed with a ray
    - added a Prefab for the base configuration in Prefabs/XR Interactables
    - Added InteractableVisual script to change outline materials when hovering or selecting

---
+++ Assets/Scripts/Graphics/Animations/InteractableVisual.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class InteractableVisual : MonoBehaviour
+{
+    private bool isGrabbed;
+
+    [Header("References")]
+    [SerializeField] private MeshRenderer meshRenderer;
+
+    [Space]
+
+    [Header("Materials")]
+    [SerializeField] private Material baseOutlineMaterial;
+
+    [Space]
+
+    [SerializeField] private Material rayHoveredOutlineMaterial;
+    [SerializeField] private Material directHoveredOutlineMaterial;
+
+    [Space]
+
+    [SerializeField] private Material raySelectedOutlineMaterial;
+    [SerializeField] private Material directSelectedOutlineMaterial;
+
+    public void StartHover(HoverEnterEventArgs args)
+    {
+        if (args.interactorObject.GetType() == typeof(XRRayInteractor))
+        {
+            SetOutline(rayHoveredOutlineMaterial);
+        }
+        else if (args.interactorObject.GetType() == typeof(XRDirectInteractor))
+        {
+            SetOutline(directHoveredOutlineMaterial);
+        }
+    }
+
+    public void StartGrab(SelectEnterEventArgs args)
+    {
+        isGrabbed = true;
+
+        if (args.interactorObject.GetType() == typeof(XRRayInteractor))
+        {
+            SetOutline(raySelectedOutlineMaterial);
+        }
+        else if (args.interactorObject.GetType() == typeof(XRDirectInteractor))
+        {
+            SetOutline(directSelectedOutlineMaterial);  
+        }
+    }
+
+    public void EndHover(HoverExitEventArgs args)
+    {
+        if (!isGrabbed)
+        {
+            ResetMaterials();
+        }
+    }
+
+    public void EndGrab(SelectExitEventArgs args)
+    {
+        ResetMaterials();
+        isGrabbed = false;
+    }
+
+    void SetOutline(Material outline)
+    {
+        Material[] materials = meshRenderer.materials;
+        materials[1] = outline;
+
+        meshRenderer.materials = materials;
+    }
+
+    void ResetMaterials()
+    {
+        SetOutline(baseOutlineMaterial);
+    }
+}
--- Assets/Scripts/XR Interactables/XRPullInteractable.cs
+++ Assets/Scripts/XR Interactables/XRPullInteractable.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class XRPullInteractable : XRBaseInteractable
+public class XRPullInteractable : XRGrabInteractable
 {
     [Space]
     [Header("Pull Settings")]
     [SerializeField] private float requiredExtraDistance;
+
+    [Space]
+    [SerializeField] private float horizontalMultiplier = 1.2f;
+    [SerializeField] private float verticalMultiplier = 4f;
+
+    [SerializeField] private float minVerticalVelocity = 2f;
 
     [Space]
 
@@ -16,26 +22,48 @@ public class XRPullInteractable : XRBaseInteractable
 
     void StartGrab(SelectEnterEventArgs args)
     {
-        startDistance = Vector3.Distance(args.interactorObject.transform.position, transform.position);
+        if (args.interactorObject.GetType() == typeof(XRRayInteractor))
+        {
+            startDistance = Vector3.Distance(args.interactorObject.transform.position, transform.position);
+            SetMovementEnabled(false);
+        }
+    }
+
+    void SetMovementEnabled(bool enabled)
+    {
+        trackPosition = enabled;
+        trackRotation = enabled;
+        throwOnDetach = enabled;
     }
 
     void EndGrab(SelectExitEventArgs args)
     {
+        if (args.interactorObject.GetType() == typeof(XRRayInteractor))
+        {
+            SetMovementEnabled(true);
+            StartCoroutine(Pull(args));
+        }
+    }
+
+    private IEnumerator Pull(SelectExitEventArgs args)
+    {
+        yield return 0;
         TryPull(args);
     }
 
     private void TryPull(SelectExitEventArgs args)
     {
         float endDistance = Vector3.Distance(args.interactorObject.transform.position, transform.position);
-        Debug.Log($"Ending Distance: {endDistance}");
 
         if (startDistance < endDistance - requiredExtraDistance)
         {
-            Vector3 pull = (args.interactorObject.transform.position - transform.position).normalized * endDistance * 2f;
+            Vector3 pull = (args.interactorObject.transform.position - transform.position).normalized * endDistance;
+            pull.x *= horizontalMultiplier;
+            pull.z *= horizontalMultiplier;
 
+            pull.y = Mathf.Max((pull.y + minVerticalVelocity) * verticalMultiplier, minVerticalVelocity);
 
             body.velocity = pull;
-            Debug.Log(pull.magnitude);
         }
 
         startDistance = 0f;
