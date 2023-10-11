4660ecabee7865221eded52b490f8a5e5791fe4b
yun-ujel <dhall2022@elam.co.uk>
2023-10-11 16:30:12 +0100

    Copied GraffitiTestScene to XRTestScene
    Also updated PropCamera to calculate score based on the raycasthit's distance to the graffiti's center

--- Assets/Scripts/Mechanics/Prop Camera/PropCamera.cs
+++ Assets/Scripts/Mechanics/Prop Camera/PropCamera.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Inkworld.Extensions;
+using TMPro;
 
 namespace Inkworld.Mechanics
 {
@@ -22,34 +23,29 @@ namespace Inkworld.Mechanics
 
         [Header("UI")]
         [SerializeField] private Slider slider;
+        [SerializeField] private TextMeshProUGUI UGUI;
 
         void FixedUpdate()
         {
             Ray direction = new Ray(transform.position, transform.forward);
             if (Physics.Raycast(direction, out RaycastHit groundHit, 100f, groundLayer))
             {
-                float maxDistance = groundHit.distance;
-                //bool hitGraffiti = Physics.BoxCast(transform.position + offset, size / 2, transform.forward, out RaycastHit graffitiHit, Quaternion.identity, maxDistance + 2f, graffitiLayer);
-                bool hitGraffiti = Physics.Raycast(direction, out RaycastHit graffitiHit, 100f, graffitiLayer);
+                float groundDistance = groundHit.distance;
+                bool hitGraffiti = Physics.Raycast(direction, out RaycastHit graffitiHit, groundDistance + 2f, graffitiLayer);
 
                 if (hitGraffiti)
                 {
-                    float distance = Vector3.Distance(graffitiHit.point, graffitiHit.transform.position);
-                    slider.value = 1f;
+                    float distance = Vector3.SqrMagnitude(graffitiHit.point - graffitiHit.transform.position);
+                    float maxDistance = Mathf.Max(graffitiHit.transform.lossyScale.x, graffitiHit.transform.lossyScale.y, graffitiHit.transform.lossyScale.z) / 2f;
+
+                    maxDistance *= maxDistance;
+
+                    slider.value = distance.Remap(0, maxDistance, 1, 0);
+
+                    UGUI.text = $"Max: {maxDistance}, Current: {distance}";
+
                     return;
                 }
-            }
-            SetSliderValue(0f);
-        }
-
-        private void SetSliderValue(float dotProduct)
-        {
-            if (dotProduct >= 0.95f)
-            {
-                float remapped = dotProduct.Remap(0.95f, 1, 0, 1);
-                slider.value = remapped;
-
-                return;
             }
             slider.value = 0f;
         }
